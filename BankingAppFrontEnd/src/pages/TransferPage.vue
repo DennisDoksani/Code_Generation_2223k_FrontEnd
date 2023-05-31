b970b0167c171bdd223e037df7b50a9bbecf4e2c
RuubioRuub <70816787+RuubioRuub@users.noreply.github.com>
2023-05-31 17:42:11 +0200

    Initial commit
    Wa

---
+++ BankingAppFrontEnd/src/pages/TransferPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <h1>Transfer</h1>
+
+        <div class="input-group mb-3">
+            <span class="input-group-text">Amount</span>
+            <input type="number" class="form-control" />
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name: 'TransferPage',
+
+}
+</script>
+
+<style></style>
--- BankingAppFrontEnd/src/router/routes.js
+++ BankingAppFrontEnd/src/router/routes.js
@@ -1,21 +1,33 @@
+const routes = [{
+        path: '/',
+        component: () =>
+            import ('layouts/BankingAppLayout.vue'),
+        children: [{
+                path: '',
+                component: () =>
+                    import ('pages/IndexPage.vue')
+            },
+            {
+                path: '/accounts',
+                component: () =>
+                    import ('pages/AccountsOverviewPage.vue')
+            },
+            {
+                path: '/transfer',
+                component: () =>
+                    import ('pages/TransferPage.vue')
+            },
 
-const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/BankingAppLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/accounts',component: () => import('pages/AccountsOverviewPage.vue') },
+        ]
+    },
 
-    ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '/:catchAll(.*)*',
+        component: () =>
+            import ('pages/ErrorNotFound.vue')
+    }
 ]
 
 export default routes
