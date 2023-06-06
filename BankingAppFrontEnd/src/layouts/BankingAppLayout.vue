ef81dd03df0485f0048e2a6f8c67d8c32ba421d5
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-06-06 19:24:22 +0200

    bijay mf

--- BankingAppFrontEnd/src/layouts/BankingAppLayout.vue
+++ BankingAppFrontEnd/src/layouts/BankingAppLayout.vue
@@ -1,8 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <h1>Navigation header</h1>
     <q-page-container>
       <router-view />
     </q-page-container>
+    <h1>Footer mf</h1>
   </q-layout>
 </template>
 
