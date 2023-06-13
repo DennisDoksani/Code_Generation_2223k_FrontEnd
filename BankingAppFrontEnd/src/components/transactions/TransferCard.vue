3725304be225f6b7f0b2b2023d1a1d13ffbbfcd6
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-06-13 22:22:05 +0200

    search by iban completed

--- BankingAppFrontEnd/src/components/transactions/TransferCard.vue
+++ BankingAppFrontEnd/src/components/transactions/TransferCard.vue
@@ -1,26 +1,38 @@
 <template>
-    <div>
-        <q-card>
+    <q-card class="" style="width:50%">
 
-            <q-card-section class="q-pa-md d-flex flex-column flex-center">
-                <q-form @submit="transfer">
-                    <div> {{ this.iban }} </div>
-                    <q-input v-model="amount" label="Amount" lazy-rules
-                        :rules="[val => !!val || 'Enter the amount you want to transfer']" />
-                    <q-input v-model="accountTo" label="Receiver" lazy-rules
-                        :rules="[val => !!val || 'Enter the account you want to transfer to']" />
-                    <q-btn type="submit" label="Confirm transfer" class="q-mt-md" />
-                </q-form>
-
-                <q-input v-model="searchName" 
-                    label="Search for an account" 
-
-                    />
-
-
-            </q-card-section>
-        </q-card>
-    </div>
+        <q-card-section class="q-pa-md">
+            <q-form @submit="transfer">
+                <h6> {{ this.iban }} </h6>
+                <q-input v-model="amount" label="Amount" lazy-rules
+                    :rules="[val => !!val || 'Enter the amount you want to transfer']" />
+                <q-input v-model="accountTo" label="Receiver" lazy-rules
+                    :rules="[val => !!val || 'Enter the account you want to transfer to']" />
+                <br>
+                <q-btn type="submit" color="green" label="Confirm transfer" class="q-mt-md" />
+            </q-form>
+        </q-card-section>
+        <q-card-section class="q-pa-md">
+                <q-select
+                    style="min-width: 250px;"
+                    label="Search account by customer name" 
+                    v-model="selectedOption"
+                    use-input 
+                    clearable 
+                    :options="options"
+                    option-label="label"
+                    option-value="iban"
+                    option-key="iban"
+                    @filter="searchAccounts"
+                    @update:model-value="updateAccountField"
+                >
+                </q-select>
+        </q-card-section>
+            
+            
+        
+        
+    </q-card>
 </template>
 
 <script>
@@ -41,7 +53,8 @@ export default {
             accountFrom: this.iban,
             accountTo: "",
             amount: 0.00,
-            searchName: "",
+            options: [],
+            selectedOption: null,
         }
     },
     methods: {
@@ -81,9 +94,51 @@ export default {
                     }
                     console.log(error);
                 });
+        },
+        searchAccounts(val, update) {
+            console.log(val)
+            if (!val) {
+                this.options.value = []
+                return
+            }
+
+            axios
+                .get("/accounts/searchByCustomerName", {
+                    params: {
+                        customerName: val,
+                        limit: 5
+                    }
+                })
+                .then((response) => {
+                    console.log(response.data);
+                    const searchedAccounts = response.data;
+                    for (const element of searchedAccounts) {
+                        element.label = element.accountHolderName + " - " + element.iban;
+                    }
+
+                    update(() => {this.options = searchedAccounts})
+                })
+                .catch((error) => {
+                    if (!error.response) {
+                        this.$q.notify({
+                            color: 'negative',
+                            message: 'Connection error',
+                            icon: 'warning',
+                            position: 'top'
+                        })
+                    }
+                    else{
+                        console.log(error.response);
+                    }
+                });
+        },
+        updateAccountField() {
+            if (this.selectedOption == null) {
+                return;
+            }
+            console.log(this.selectedOption);
+            this.accountTo = this.selectedOption.iban;
         }
     }
 }
 </script>
-
-<style></style>
